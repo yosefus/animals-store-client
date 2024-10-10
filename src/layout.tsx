289fc70778ffbb003a5e1629131df1f7647d80d6
yosefus <yosalsoni@gmail.com>
2024-10-10 14:28:28 +0300

    analytics

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Header from "./components/header"
+import { Analytics } from "@vercel/analytics/react"
 
 function Layout() {
    return (
@@ -8,6 +9,7 @@ function Layout() {
          <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
             <Outlet/>
          </main>
+         <Analytics/>
       </div>
    )
 }
